c2c25ca4c69c3d06ee8aaeacbf158802bd29d49d
root <root@DESKTOP-OS1JJFM.localdomain>
2025-11-13 15:30:20 +0300

    update repository.py  add ssl mode

--- page_analyzer/repository.py
+++ page_analyzer/repository.py
@@ -3,7 +3,13 @@ import psycopg
 from psycopg.rows import dict_row
 
 def get_db(app):
-    return psycopg.connect(app.config['DATABASE_URL'])
+    database_url = app.config['DATABASE_URL']
+    if database_url.startswith('postgresql://'):
+        if 'sslmode' not in database_url:
+            database_url += "?sslmode=require"
+    
+    return psycopg.connect(database_url)
+
 
 
 class UrlsRepository:
