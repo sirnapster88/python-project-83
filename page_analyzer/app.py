e1f132e5818b2298b8347b34a1f8ffe80b2eecec
root <root@DESKTOP-OS1JJFM.localdomain>
2025-11-13 08:54:53 +0300

    add templates

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, render_template
 
 load_dotenv()
 app = Flask(__name__)
@@ -9,7 +9,7 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
