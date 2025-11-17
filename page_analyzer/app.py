0eec3bdc480017bd434aa61e5483b125695ba820
sirnapster88 <sir.napster@yandex.ru>
2025-11-17 23:28:02 +0300

    reverse to unwork

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -44,10 +44,10 @@ def create_url_from_index():
     if errors:
         for error in errors.items():
             flash(f"{error}", "error")
-        if request.path == "/urls":
-            return render_template("urls.html", url_data=url_data, errors=errors), 422  # noqa: E501
-        else:
+        if request.path == "/":
             return render_template("index.html", url_data=url_data, errors=errors), 422  # noqa: E501
+        else:
+            return render_template("urls.html", url_data=url_data, errors=errors), 422  # noqa: E501
 
     saved_id = repo.save(url_data)
 
